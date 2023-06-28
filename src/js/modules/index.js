54725c2b545f24d2f432b81208c5f4c29986645d
IvanBilchenko1 <Bilchenko035@gmail.com>
2023-06-28 17:54:51 -0400

    Slider
    Added slider

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -230,6 +230,76 @@ $(document).ready(function() {
       $('.item').on('click', toggleAccordion);
   });
 
+  $(document).ready(function(){
+    $('.carousel').slick({
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 2,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots:true,
+      arrows: false,
+      centerMode: true,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          // centerMode: true,
+  
+        }
+  
+      }, {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true,
+          infinite: true,
+        }
+      },
+
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+          dots: true,
+          infinite: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        }
+      },
+
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        }
+      }]
+    });
+  });
+
+// keep the same height for slider
+  $('.carousel').on('setPosition', function() {
+    var slides = $('.card');
+    var maxHeight = 0;
+  
+    slides.css('height', ''); // Reset slide heights
+  
+    slides.each(function() {
+      var slideHeight = $(this).outerHeight();
+      maxHeight = Math.max(maxHeight, slideHeight);
+    });
+  
+    slides.css('height', maxHeight); // Set equal height to all slides
+  });
+
 export {
   isWebp,
   homeRedirect,
